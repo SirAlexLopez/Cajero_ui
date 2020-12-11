ddbb5ba57316a187ae563a7a35e9bce8091b655d
SirAlexLopez <johan.lopez.ortiz@gmail.com>
2020-12-10 20:19:39 -0500

    files frontend cajero

---
+++ src/components/Componente.vue
@@ -0,0 +1,16 @@
+<template> 
+  <h1 id="componente"> Soy un componente </h1> 
+</template>
+
+<script>
+  export default {
+     name: "Componente", 
+} 
+</script>
+
+<style>
+   
+   #componente{ 
+     color:blue 
+} 
+</style>
